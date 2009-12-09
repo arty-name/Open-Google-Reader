60c45a6c7cfbb7656179b648b29b2d85fd2e45a2
arty <arty@rememberthemilk.com>
2009-12-10 02:30:18 +0300

    add license data to meta

--- build/open-google-reader.js
+++ build/open-google-reader.js
@@ -5,6 +5,7 @@
 // @author       Artemy Tregubenko <me@arty.name>
 // @description  Replaces native Google Reader's interface with fully customizable one.
 // @homepage     http://github.com/arty-name/Open-Google-Reader
+// @license      Simplified BSD License; http://en.wikipedia.org/wiki/Bsd_license#2-clause_license_.28.22Simplified_BSD_License.22_or_.22FreeBSD_License.22.29
 // @include      http://www.google.com/reader/view/
 // @include      http://www.google.com/reader/view/1
 // @run-at       document-start
