f886c2ce840ba584df62f2850a5073689c63fc7f
arty <me@arty.name>
2011-04-10 09:23:26 +0400

    moved source files to src/

--- build/open-google-reader.user.js
+++ build/open-google-reader.user.js
@@ -1821,6 +1821,32 @@ function lib() {
   }
 
 }
+
+function onload() {
+  lib();
+  ui();
+}
+
+if (typeof GM_xmlhttpRequest != "undefined" && !navigator.userAgent.match(/Chrome/)) {
+  // GreaseMonkey, fuck you very much! I don't need your overprotection.
+  var script = document.createElement('script');
+  script.innerHTML =
+    defineSettings.toString() + lib.toString() + ui.toString() +
+    'defineSettings(); lib(); ui();';
+  document.body.appendChild(script);
+  setTimeout(function(){
+    var style = document.createElement('style');
+    style.innerHTML = settings.css;
+    document.body.previousElementSibling.appendChild(style);
+  }, 100);
+  
+} else if (document.readyState.match(/complete|loaded/)) {
+  onload();
+  
+} else {
+  window.addEventListener('DOMContentLoaded', onload, false);
+}
+
 window.opera && window.opera.addEventListener(
   'BeforeExternalScript',
   function(event){
