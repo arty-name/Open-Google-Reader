d305d934e62d5f0f70934e7ada89a35ee2a40607
arty <me@arty.name>
2012-02-11 11:36:59 +0100

    fixed indentation

--- src/25settings.js
+++ src/25settings.js
@@ -2,26 +2,26 @@ defineSettings();
 
 function defineSettings() {
 
-settings = {
+  settings = {
 
-  // only required to be loaded on 404 url, i.e. http://www.google.com/reader/view/1
-  userId: '',
-  
-  // lowercase words to filter entries out by title
-  titleFilters: [],
-  
-  // words to filter entries out by body (html included)
-  bodyFilters: [],
-  
-  // your device screen's horizontal resolution
-  mobileViewPort: '800x480',
-  
-  // filters to manipulate on entry content
-  // NB: set data.altered = true if you want these changes to be shared when you click "share"
-  entryHtmlAlterations: [],
-  entryDomAlterations: []
-  
-};
+    // only required to be loaded on 404 url, i.e. http://www.google.com/reader/view/1
+    userId: '',
+
+    // lowercase words to filter entries out by title
+    titleFilters: [],
+
+    // words to filter entries out by body (html included)
+    bodyFilters: [],
+
+    // your device screen's horizontal resolution
+    mobileViewPort: '800x480',
+
+    // filters to manipulate on entry content
+    // NB: set data.altered = true if you want these changes to be shared when you click "share"
+    entryHtmlAlterations: [],
+    entryDomAlterations: []
+
+  };
 
   var parts = settings.mobileViewPort.split('x');
   settings.mobileViewPort = {
