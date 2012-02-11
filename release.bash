e4ebd151c07174d6b5338a337e7d2cb3e6e714f2
arty <me@arty.name>
2012-02-11 11:48:52 +0100

    simplified build script more

--- release.bash
+++ release.bash
@@ -5,15 +5,17 @@ sed 's/^/"/;s/$/" +/;' src/styles.css >> 27styles
 sed 's/^/"/;s/$/" +/;' src/styles.custom.css >> 27styles
 echo '"";' >> 27styles
 
-cat src/10meta.js > build/open-google-reader.js
-cat src/20prefix >> build/open-google-reader.js
-cat src/25settings.js >> build/open-google-reader.js
-cat 27styles >> build/open-google-reader.js
-cat src/30ui.js >> build/open-google-reader.js
-cat src/40lib.js >> build/open-google-reader.js
-cat src/50onload.js >> build/open-google-reader.js
-cat src/60killDefault.js >> build/open-google-reader.js
-cat src/70postfix >> build/open-google-reader.js
+cat \
+  src/10meta.js \
+	src/20prefix \
+	src/25settings.js \
+	27styles \
+	src/30ui.js \
+	src/40lib.js \
+	src/50onload.js \
+	src/60killDefault.js \
+	src/70postfix \
+	> build/open-google-reader.js
 
 cp build/open-google-reader.js build/open-google-reader.user.js
 
